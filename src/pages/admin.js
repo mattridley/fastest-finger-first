f46cab4f281e8de5f3009cb5de7d4e13cadcef7e
Matt Ridley <matt.c.ridley@gmail.com>
2020-12-28 18:03:21 +0000

    Add additional questions

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -24,6 +24,53 @@ const questions = [
     },
     correctAnswer: ["c", "a", "b", "d"],
   },
+  {
+    id: 2,
+    question:
+      "Starting with the earliest, put these forms of transport in the order they were introduced",
+    options: {
+      a: "Airbus A380",
+      b: "Flying Scotsman",
+      c: "Model T Ford",
+      d: "Penny-fathing",
+    },
+    correctAnswer: ["d", "c", "b", "a"],
+  },
+  {
+    id: 3,
+    question: "Put these UK National Parks in order from north to south",
+    options: {
+      a: "Carnigorms",
+      b: "Brecon Beacons",
+      c: "Dartmoor",
+      d: "Yorkshire Dales",
+    },
+    correctAnswer: ["a", "d", "b", "c"],
+  },
+  {
+    id: 4,
+    question:
+      "Starting with the earliest, put these life stages of a butterfly in order",
+    options: {
+      a: "Adult",
+      b: "Caterpillar",
+      c: "Egg",
+      d: "Chrysalis",
+    },
+    correctAnswer: ["c", "b", "d", "a"],
+  },
+  {
+    id: 5,
+    question:
+      "Starting with with the earliest, put these TV cooks in the order they were born",
+    options: {
+      a: "Delia Smith",
+      b: "Nadiya Hussain",
+      c: "Mary Berry",
+      d: "Nigella Lawson",
+    },
+    correctAnswer: ["c", "a", "d", "b"],
+  },
 ];
 
 export default function Admin() {
@@ -64,9 +111,9 @@ export default function Admin() {
 
   return (
     <>
-      <button onClick={clearQuestion}>Clear Question</button>
       {questions.map(({ id, question, options, correctAnswer }) => (
         <div key={id}>
+          <h3>Q{id + 1}</h3>
           <p>{question}</p>
           <p>
             {correctAnswer
@@ -76,7 +123,9 @@ export default function Admin() {
           <div>
             <button onClick={() => sendQuestion(id)}>Send Question</button>
             <button onClick={() => sendAnswer(id)}>Send Answer</button>
+            <button onClick={clearQuestion}>Clear Question</button>
           </div>
+          <hr />
         </div>
       ))}
     </>
